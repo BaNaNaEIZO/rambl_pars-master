f52be55c849e4f29bf7c0ccde4507f148846c0b5
BaNaNaEIZO <n14.hunter.911@yandex.ru>
2024-01-12 01:24:21 +0700

    Update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from correlation import Correlation
 
 
 if __name__ == '__main__':
-    # parser = RamblerPars()
-    # parser.page_request()
+    parser = RamblerPars()
+    parser.page_request()
     correlation = Correlation()
     correlation.correlation()
