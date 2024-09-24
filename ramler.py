1dee5b25d0f1c2a7a8ab128a10bb353838a68bca
BaNaNaEIZO <n14.hunter.911@yandex.ru>
2024-09-24 19:49:42 +0700

    before add k-mean

--- ramler.py
+++ ramler.py
@@ -2,6 +2,7 @@ import requests
 import datetime
 import json
 import csv
+import re
 
 
 # Парсер выполняет поиск по ссылке https://peroxide.rambler.ru/v1/projects/1/clusters/?limit=50&page=1&date=2023-12-31
@@ -51,9 +52,9 @@ class RamblerPars:
         with open(self.tag_file, mode="r", encoding="utf-8") as r_file:
             data = r_file.read()
             data = json.loads(data)
-            for item in data:
-                tags = data[item].split(", ")
-                data[item] = tags
+            # for item in data:
+            #     tags = data[item].split(", ")
+            #     data[item] = tags
         return data
 
     # Запрос к сайту и запись данных в файл
@@ -61,49 +62,55 @@ class RamblerPars:
         with open(self.output, mode="w", encoding=self.encoding) as w_file:
             file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
             file_writer.writerow(["week", "tag", "sum_news"])
-            all_tags = []
+            tag_list = []
             current_week = self.current_time.isocalendar().week
             # value_annotation1 = "аэропорт"
             for key in self.tags.keys():
-                all_tags.append(key)
+                tag_list.append(key)
 
-            news_from_week = [x * 0 for x in range(len(all_tags))]
+            news_from_week = [x * 0 for x in range(len(tag_list))]
 
             for weeks, url in self.get_date_request():
-                data_json = requests.get(url, self.headers)
-                data = json.loads(data_json.text)
+                try:
+                    data_json = requests.get(url, self.headers)
+                    data = json.loads(data_json.text)
+                except:
+                    print(data_json.text, data_json)
 
                 if current_week != weeks:
                     i = 0
                     print("Неделя: " + str(current_week) + " " + str(news_from_week))
                     for row in news_from_week:
-                        file_writer.writerow([current_week, all_tags[i], row])
+                        file_writer.writerow([current_week, tag_list[i], row])
                         i += 1
                     current_week = weeks
-                    news_from_week = [x * 0 for x in range(len(all_tags))]
+                    news_from_week = [x * 0 for x in range(len(tag_list))]
 
                 if len(data) != 0:
+                    temp_list = []
+
                     for i in range(len(data)):
                         value_annotation = data[i]["long_title"]
-                        print(f"\"{value_annotation}\", ")
-                        count_tag = 0
-                        for tag in all_tags:
-                            for j in self.tags[tag]:
-                                try:
-                                    if value_annotation.find(j) > 0:
-                                        news_from_week[count_tag] += 1
-                                        # print(url)
-                                        # print("Календарная неделя: " + weeks)
-                                        # print("Тэг: " + tag + " Номер тэга: " + str(count_tag))
-                                        # print("Новость: " + value_annotation)
-                                        # print("Найденое ключевое слово: " + j)
-                                        # print(value_annotation.lower().find(j))
-                                except:
-                                    pass
-                                    # print("skip")  # pass
+                        key_and_values_list = []
+                        for tag in tag_list:
+                            for item in self.tags.get(tag):
+                                temp_list.append([tag, item[0], item[1]])
+                            key_and_values_list.append(temp_list)
+                            temp_list = []
+                        var = self.search(value_annotation, key_and_values_list)
+                        if var >= 0:
+                            news_from_week[var] += 1
 
-                            count_tag += 1
-                    # print(news_from_week)
+    def search(self, in_str, key_and_values_list):
+        news_weight_list = [x * 0 for x in range(len(key_and_values_list) + 1)]
+        for my_str in re.split(r'[ ,«»":()]+', in_str):
+            count = 1
+            for item in key_and_values_list:
+                for value in item:
+                    if my_str.lower() == value[1]:
+                        news_weight_list[count] += value[2]
+                count += 1
+        return (max(enumerate(news_weight_list), key=lambda x: x[1])[0]) - 1
 
     def get_date_request(self):
         for current_time in self.date_list:
